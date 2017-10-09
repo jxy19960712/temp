136419d2b5e337f29b7365b022d550802b374154
spikespiegel5112 <583288277@qq.com>
2017-10-09 18:30:05 +0800

    temp

--- vue-app/src/components/leadershipOverview/charts/nationStructure.vue
+++ vue-app/src/components/leadershipOverview/charts/nationStructure.vue
@@ -14,19 +14,19 @@
     import data from './life-expectancy.json';
     import * as d3 from '../../../assets/js/d3.min.js';
     //模拟数据
-    const charistic = {
+    var charistic = {
         children: [{
-            name: '多血质200',
-            value: 200
+            name: '多血质400',
+            value: 400
         }, {
             name: '多血质100',
             value: 100
         }, {
+            name: '多血质20',
+            value: 20
+        }, {
             name: '多血质150',
             value: 150
-        }, {
-            name: '多血质50',
-            value: 50
         }]
     }
     export default {
@@ -50,15 +50,29 @@
                     pack = d3.layout.pack().size([width, height]),
                     color = d3.scale.category20();
 
+                //对数据进行排序
+                for(var unfix=charistic.children.length-1; unfix>0; unfix--){
+                    for(var i=0; i<unfix;i++){
+                        if(charistic.children[i].value>charistic.children[i+1].value){
+                            var temp = charistic.children[i];
+                            charistic.children.splice(i,1,charistic.children[i+1]);
+                            charistic.children.splice(i+1,1,temp);
+                        }
+                    }
+                }
+                console.log(charistic.children)
+
                 svg.selectAll('circle').data(pack.nodes(charistic)).enter().append('circle')
                     .attr('cx', function (d) {
-                        console.log(d)
                         return d.x;
                     }).attr('cy', function (d) {
                     return d.y;
                 }).attr('r', function (d) {
                     return d.r;
                 }).attr('fill', function (d, i) {
+                    if(i==0){
+                        return;
+                    }
                     that.legendTitleList.push({
                         legendColor: color(i),
                         legendTitle: ''
@@ -68,17 +82,16 @@
                     return d.value
                 })
 
-                for (var index in charistic.children+1) {
-//                    alert(charistic.children.length-index)
-                    if(index==0){
-                        continue;
-                    }else{
-                        this.legendTitleList[index].legendTitle = charistic.children[charistic.children.length-index].name
-                    }
-//                    this.legendTitleList[index].legendTitle = charistic.children[charistic.children.length-index-1].name
-
+                for(var index=1;index<=charistic.children.length;index++){
+                    this.legendTitleList[index].legendTitle = charistic.children[index-1].name
+//                    if(i==0){
+//                        continue;
+//                    }else{
+//                        debugger
+//                        this.legendTitleList[index].legendTitle = charistic.children[charistic.children.length-index].name
+//                    }
                 }
-
+//                console.log(this.legendTitleList)
             }
         }
     }
